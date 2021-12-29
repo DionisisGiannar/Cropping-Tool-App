daea098d407e085fe05cd035c554aa76404c22a7
Dionisis Giannaropoulos <74465424+DionisisGiannar@users.noreply.github.com>
2021-12-29 02:27:03 +0200

    some more bugs 29/12/2021

--- src/CroppingPack1/CropAppFrame.java
+++ src/CroppingPack1/CropAppFrame.java
@@ -1,34 +1,66 @@
 package CroppingPack1;
 
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.io.File;
-
+import java.util.Arrays;
+import java.util.List;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.Color;
-import java.awt.Label;
 import java.awt.Font;
 import javax.swing.JPanel;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.JSlider;
+import javax.swing.JMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.FlowLayout;
 
 
-public class CropAppFrame extends JFrame implements ActionListener{ //Creates a Window Frame for the App
+public class CropAppFrame extends JFrame implements ActionListener, ChangeListener{ //Creates a Window Frame for the App
 
 	private static final long serialVersionUID = 1L;
 
 	final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // Screen Size
+	List<String> typeFiles = Arrays.asList(".jpg", ".jpeg", ".png", ".tif", ".tiff");
 	ImageLabel imgLabel;
-
-
+	JButton srchFileButton;
+	JButton btnSaveAs, btnNewImage;
+	JLabel txtLabel;
+	WidthHeightLabel wHLabel;
+	
+	String filePath, folderPath;
+	
+	private JSlider sldrWidth;
+	private JLabel lblNumbWidth;
+	private JPanel pnlControls;
+	private JLabel lblTxtHeight;
+	private JLabel lblNumbHeight;
+	private JSlider sldrHeight;
+	private JLabel lblTxtX;
+	private JLabel lblNumbX;
+	private JSlider sldrX;
+	private JSlider sldrY;
+	private JLabel lblTxtY;
+	private JLabel lblNumbY;
+	JPanel pnlButtons;
+	
+	/**
+	 * constructor
+	 */
 	public CropAppFrame() {
 		getContentPane().setBackground(Color.LIGHT_GRAY);
 		//this.setVisible(true);
@@ -42,84 +74,400 @@ public class CropAppFrame extends JFrame implements ActionListener{ //Creates a 
 		getContentPane().setLayout(null);
 		getContentPane().setSize(new Dimension(1920, 1080));
 		this.setVisible(true);
-		 
-		 JPanel srchFileBtnPanel = new JPanel();
-		 srchFileBtnPanel.setForeground(Color.WHITE);
-		 srchFileBtnPanel.setBackground(new Color(0,0,0,0));
-		 srchFileBtnPanel.setBounds(920, 500, 93, 35);
-		 
-		 getContentPane().add(srchFileBtnPanel);
-		 
-		 JButton srchFileButton = new JButton("Search");
-		 
-		 
-		 srchFileBtnPanel.add(srchFileButton);
-		 
-		 JLabel txtLabel = new JLabel("Browse for an Image");
-		 txtLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		 txtLabel.setText("Browse for an Image");
-		 txtLabel.setFont(new Font("MathJax_Typewriter", Font.PLAIN, 16));
-		 txtLabel.setForeground(Color.BLACK);
-		 
-		 txtLabel.setBounds(871, 473, 193, 21);
-		 getContentPane().add(txtLabel);
-
-		//image Label
-		imgLabel = new ImageLabel();
-		imgLabel.setSize(1835, 934);
-		imgLabel.setLocation(73, 69);
-		
-		
-		getContentPane().add(imgLabel);
-		
-		//Dimensions Label
-		WidthHeightLabel wHLabel = new WidthHeightLabel(imgLabel);
-		wHLabel.setVisible(false);
-		wHLabel.setBounds(73, 10, 1773, 53);
-		
-		getContentPane().add(wHLabel);
-		repaint();
 		
 		//File Search Button
+		srchFileButton = new JButton("Search");
+		srchFileButton.setBounds(920, 500, 90, 30);
+		srchFileButton.setFont(new Font("MathJax_Typewriter", Font.BOLD, 14));
+		srchFileButton.setActionCommand("searchFile");
+		srchFileButton.addActionListener(this);
+		//text label
+		txtLabel = new JLabel("Browse for an Image");
+		txtLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		txtLabel.setText("Browse for an Image");
+		txtLabel.setFont(new Font("MathJax_Typewriter", Font.PLAIN, 16));
+		txtLabel.setForeground(Color.BLACK);	
+		txtLabel.setBounds(851, 477, 225, 21);
+		
+		getContentPane().add(txtLabel);
+		getContentPane().add(srchFileButton);
+		
+		//Buttons
+		pnlButtons = new JPanel();
+		pnlButtons.setBounds(783, 978, 399, 37);
+		pnlButtons.setBackground(new Color(0,0,0,0) );
+		pnlButtons.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		//New Image Button
+		btnNewImage = new JButton("New Image");
+		pnlButtons.add(btnNewImage);
+		btnNewImage.setActionCommand("new");
+		btnNewImage.addActionListener(this);
+		//save as button
+		btnSaveAs = new JButton("Save as");
+		pnlButtons.add(btnSaveAs);
+		btnSaveAs.setActionCommand("saveAs");
+		btnSaveAs.addActionListener(this);
+		//crop image button
+		JButton btnCrop = new JButton("Crop The Image");
+		btnCrop.setActionCommand("crop");
+		pnlButtons.add(btnCrop);
+		btnCrop.addActionListener(this);
+		
+		//Control Panel
+		pnlControls = new JPanel();
+		pnlControls.setBorder(BorderFactory.createLineBorder(Color.black));
+		pnlControls.setBounds(12, 66, 200, 909);
+		pnlControls.setBackground(new Color(0,0,0,0));
+		
+		pnlControls.setLayout(null);
+		
+		//SLIDER SIZE
+		sldrWidth = new JSlider(0,1920,0);
+		sldrWidth.setBackground(new Color(0,0,0,0));
+		sldrWidth.setBounds(12, 59, 176, 16);
+        // setChangeListener
+		sldrWidth.addChangeListener(this);
+		
+		pnlControls.add(sldrWidth);
 		
 		
-		/*
-		RectanglePanel rectPanel = new RectanglePanel();
-		rectPanel.setSize(1884, 1007);
-		rectPanel.setLocation(10, 11);
+		JLabel lblTxtWidth = new JLabel("Width:");
+		lblTxtWidth.setFont(new Font("MathJax_Typewriter", Font.PLAIN, 14));
+		lblTxtWidth.setBounds(12, 24, 51, 23);
+		pnlControls.add(lblTxtWidth);
 		
-		getContentPane().add(rectPanel);
+		lblNumbWidth = new JLabel();
+		lblNumbWidth.setBackground(new Color(0,0,0,0));
+		lblNumbWidth.setFont(new Font("C059", Font.PLAIN, 15));
+		lblNumbWidth.setBounds(60, 24, 70, 22);
+		
+		// set the text of label
+		//lblNumbWidth.setText(""+ sldrWidth.getValue());
+		pnlControls.add(lblNumbWidth);
+		
+		//Slider for Height
+		sldrHeight = new JSlider(0, 1920, 0);
+		sldrHeight.setBackground(new Color(0, 0, 0, 0));
+		sldrHeight.setBounds(12, 122, 176, 16);
+		sldrHeight.addChangeListener(this);
+		
+		pnlControls.add(sldrHeight);
+		
+		lblTxtHeight = new JLabel("Height:");
+		lblTxtHeight.setFont(new Font("MathJax_Typewriter", Font.PLAIN, 14));
+		lblTxtHeight.setBounds(12, 87, 59, 23);
+		pnlControls.add(lblTxtHeight);
+		
+		lblNumbHeight = new JLabel();
+		lblNumbHeight.setFont(new Font("C059", Font.PLAIN, 15));
+		lblNumbHeight.setBackground(new Color(0, 0, 0, 0));
+		lblNumbHeight.setBounds(70, 87, 70, 22);
+		pnlControls.add(lblNumbHeight);
+		
+		//Slider for X coordinate
+		sldrX = new JSlider(0, 1920, 0);
+		sldrX.setBackground(new Color(0, 0, 0, 0));
+		sldrX.setBounds(12, 174, 176, 16);
+		sldrX.addChangeListener(this);
+		pnlControls.add(sldrX);
+		
+		lblTxtX = new JLabel("X :");
+		lblTxtX.setFont(new Font("MathJax_Typewriter", Font.PLAIN, 14));
+		lblTxtX.setBounds(12, 150, 27, 23);
+		pnlControls.add(lblTxtX);
+		
+		lblNumbX = new JLabel();
+		lblNumbX.setFont(new Font("C059", Font.PLAIN, 15));
+		lblNumbX.setBackground(new Color(0, 0, 0, 0));
+		lblNumbX.setBounds(39, 150, 70, 22);
+		pnlControls.add(lblNumbX);
+		
+		//Slider for Y coordinate
+		sldrY = new JSlider(0, 1920, 0);
+		sldrY.setForeground(Color.BLACK);
+		sldrY.setBackground(new Color(0, 0, 0, 0));
+		sldrY.setBounds(12, 231, 176, 16);
+		sldrY.addChangeListener(this);
+		pnlControls.add(sldrY);
+		
+		lblTxtY = new JLabel("Y :");
+		lblTxtY.setFont(new Font("MathJax_Typewriter", Font.PLAIN, 14));
+		lblTxtY.setBounds(12, 203, 27, 23);
+		pnlControls.add(lblTxtY);
+		
+		lblNumbY = new JLabel();
+		lblNumbY.setFont(new Font("C059", Font.PLAIN, 15));
+		lblNumbY.setBackground(new Color(0, 0, 0, 0));
+		lblNumbY.setBounds(30, 204, 70, 22);
+		pnlControls.add(lblNumbY);
 		
 		
-		this.addMouseListener(this);
-		this.addMouseMotionListener(this);
-		*/
+		/**
+		 * for design purposes
+		 */
+		
+		//image Label
+		imgLabel = new ImageLabel(null);
+		imgLabel.setSize(1684, 909);
+		imgLabel.setLocation(224, 66);
+		//Dimensions Label
+		wHLabel = new WidthHeightLabel(imgLabel);
+		wHLabel.setVisible(true);
+		wHLabel.setBounds(73, 10, 1773, 53);
+		getContentPane().add(wHLabel);
+		getContentPane().add(imgLabel);
+		getContentPane().add(pnlControls);
+		
+		
+
+		
+		
+		
+		repaint();
 	
 		SwingUtilities.updateComponentTreeUI(this);
+		
+		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		imgLabel.fileName = getFileName();
-		checkFileName();
+		if(e.getActionCommand().equals("searchFile")) {
+			filePath = getFilePath();
+			checkFileName();
+		} 
+		if(e.getActionCommand().equals("saveAs")) {
+			imgLabel.folderPath = getFolderPath();
+			this.folderPath = imgLabel.folderPath;
+			imgLabel.CroppTheImage();
+			
+			setCroppedImg();
+		}
+		if(e.getActionCommand().equals("new")) {
+			imgLabel.setLocalCoordinates(0,0,0,0);
+			setSlidersValue();
+			removeImageAndDimLabel();
+			filePath = getFilePath();
+			setImageAndDimLabel(filePath);
+		}
+		if(e.getActionCommand().equals("crop")) {
+			imgLabel.displayCropped();
+			imgLabel.setLocalCoordinates(0,0,0,0);
+			setSlidersValue();
+		}
+	}		
+	/*
+	 * Pop Up to display the cropped Image
+	 */
+	public void setCroppedImg() {
+		int n = JOptionPane.showConfirmDialog(
+			    this,
+			    "Would you like to display the cropped image?",
+			    "Question",
+			    JOptionPane.YES_NO_OPTION);
+		//displayCroppedImgSubFrame saveSubFrame = new displayCroppedImgSubFrame(this);
+		if(n == JOptionPane.YES_OPTION){
+			imgLabel.setLocalCoordinates(0,0,0,0);
+			setSlidersValue();
+			removeImageAndDimLabel();
+			setImageAndDimLabel(imgLabel.folderPath);
+		}
+		
 	}
 	
+	
+	
+	/**
+	 * Checks if the filePath Exists.
+	 * If it exists disables the components that needs and creates the Image Label.
+	 * Else Shows message to try again.
+	 */
 	public void checkFileName() {
 		
+			for(String tF :typeFiles) {
+				if(filePath.contains(tF)) {
+					srchFileButton.setVisible(false);
+					srchFileButton.setEnabled(false);
+					
+					txtLabel.setVisible(false);
+					txtLabel.setEnabled(false);
+					
+					setImageAndDimLabel(filePath);
+					
+					break;
+				} else {
+					txtLabel.setText("Image Not Found, try again.");
+					imgLabel.drawImg.drawScaledImage(imgLabel.image, imgLabel, imgLabel.getGraphics());
+				}
+			}
+			
+			//setImageAndDimLabel();
+			//imgLabel.drawImg.drawScaledImage(imgLabel.image, imgLabel, imgLabel.getGraphics());
+	}
+	
+	/**
+	 * sets the Image and Dimensions Components and Controls
+	 * @param flPath
+	 */
+	public void setImageAndDimLabel(String flPath) {
+		//image Label
+		imgLabel = new ImageLabel(flPath);
+		imgLabel.setSize(1684, 909);
+		imgLabel.setLocation(224, 66);
+		
+		//extra mouse listeners for the slider
+		imgLabel.addMouseMotionListener(new MouseMotionListener() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				setSlidersValue();
+				
+			}
+			@Override
+			public void mouseMoved(MouseEvent e) {}
+		});
+		imgLabel.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setSlidersValue();
+
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {}
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			
+		});
+		
+
+		getContentPane().add(imgLabel);
+		
+		//Dimensions Label
+		wHLabel = new WidthHeightLabel(imgLabel);
+		wHLabel.setVisible(true);
+		wHLabel.setBounds(73, 10, 1773, 53);
+		System.out.println("//////xA="+imgLabel.xA);
+		
+		getContentPane().add(pnlControls);
+		getContentPane().add(pnlButtons);
+		getContentPane().add(wHLabel);
+		repaint();
+	}
+	
+	/**
+	 * sets all the sliders value
+	 */
+	public void setSlidersValue() {
+		setSlidersMaxMin();
+		//width
+		sldrWidth.setValue(imgLabel.coord.getWidth());
+		//height
+		sldrHeight.setValue(imgLabel.coord.getHeight());
+		//X
+		sldrX.setValue(imgLabel.coord.getX());
+		//Y
+		sldrY.setValue(imgLabel.coord.getY());
+		
+	}
+	public void setSlidersMaxMin() {
+		//width
+		sldrWidth.setMaximum(imgLabel.drawImg.x2-imgLabel.xA);
+		//height
+		sldrHeight.setMaximum(imgLabel.drawImg.y2-imgLabel.yA);
+		//X
+		sldrX.setMaximum(imgLabel.drawImg.x2-imgLabel.width);
+		sldrX.setMinimum(imgLabel.drawImg.x1);
+		//Y
+		sldrY.setMaximum(imgLabel.drawImg.y2-imgLabel.height);
+		sldrY.setMinimum(imgLabel.drawImg.y1);
+	}
+	
+	 /*
+	  * if JSlider value is changed
+	  */
+    public void stateChanged(ChangeEvent e) {
+        //for Width components
+    	lblNumbWidth.setText(""+sldrWidth.getValue());
+        imgLabel.width = sldrWidth.getValue();
+        //for Height components
+        lblNumbHeight.setText(""+sldrHeight.getValue());
+        imgLabel.height = sldrHeight.getValue();
+        
+        //for X coordinate Component
+        lblNumbX.setText(""+ (sldrX.getValue() - imgLabel.drawImg.x1) );
+        imgLabel.xA = sldrX.getValue();
+        
+        //for Y coordinate Component
+        lblNumbY.setText(""+ (sldrY.getValue() - imgLabel.drawImg.y1) );
+        imgLabel.yA = sldrY.getValue();
+        
+        //Dimensions update 
+        wHLabel.setTheText(sldrWidth.getValue(),sldrHeight.getValue());
+        
+        setSlidersMaxMin();
+        
+        //repaints
+        imgLabel.repaint();
+        lblNumbWidth.repaint();
+        lblNumbHeight.repaint();
+        repaint();
+        
+        
+    }
+    
+	/**
+	 * removes the Image and Dimensions Components
+	 */
+	public void removeImageAndDimLabel(){
+		this.imgLabel.setVisible(false);
+		this.imgLabel.setEnabled(false);
+		this.wHLabel.setVisible(false);
+		this.wHLabel.setEnabled(false);
+		this.getContentPane().remove(imgLabel);
+		this.getContentPane().remove(wHLabel);
 	}
 	
 	/**
 	 * Opens the file Browser
 	 * @return the file name the  user choose
 	 */
-	public String getFileName() {
+	public String getFilePath() {
 		JFileChooser chooser = new JFileChooser();
 		chooser.showOpenDialog(null);
 		File file = chooser.getSelectedFile();
-		String filename = file.getName();
-		System.out.println("You have selected: " + filename);
+		String filename;
+		String filePath=null; 
+		if(file==null) {
+			filePath = imgLabel.imagePath;
+		} else {
+			filename = file.getName();
+			filePath = file.getPath();
+			System.out.println("\nYou have selected: " + filename +"\nPath: "+ filePath +"\n");
+			//chooseFileButton.setVisible(false);
+		}
+		
+		
+		return filePath;
+	}
+	
+	/**
+	 * Opens folder Browser
+	 * @return the folder path the user choose
+	 */
+	public String getFolderPath() {
+		JFileChooser chooser = new JFileChooser();
+		chooser.showOpenDialog(null);
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		File folder = chooser.getSelectedFile();
+		String folderName = folder.getName();
+		String foldrPath = folder.getPath();
+		System.out.println("\nYou have selected: " + folderName+"\nPath: "+ foldrPath +"\n");
 		//chooseFileButton.setVisible(false);
 		
-		return filename;
+		return foldrPath;
 	}
 }
--- src/CroppingPack1/ImageLabel.java
+++ src/CroppingPack1/ImageLabel.java
@@ -1,6 +1,5 @@
 package CroppingPack1;
 
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -11,16 +10,17 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.awt.image.CropImageFilter;
-import java.awt.image.FilteredImageSource;
+import java.awt.image.BufferedImage;
 import java.io.File;
-
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.SwingUtilities;
-import javax.swing.SwingConstants;
+import java.lang.Math;
+
 
 
 public class ImageLabel extends JLabel implements ActionListener, MouseListener, MouseMotionListener{
@@ -28,35 +28,49 @@ public class ImageLabel extends JLabel implements ActionListener, MouseListener,
 	private static final long serialVersionUID = 1L;
 	
 	final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();// screenSize
-	
-	String fileName;
+	List<String> typeFiles = Arrays.asList(".jpg", ".jpeg", ".png", ".tif", ".tiff");
+
+	ImageDraw drawImg; // initialize object that draws the image
+	BufferedImage origImage;
 	ImageIcon icon; // initialize the icon
 	Image image; // initialize the image
 	Image croppedImage; //initialize the cropped Image
-	Coordinates coord = new Coordinates(); //Object for the Coordinates
+	Coordinates coord = new Coordinates(0,0,0,0); //Object for the Coordinates
+	Coordinates correctCoord = new Coordinates();
+	boolean isRepainted = false;
+	int x1, y1, x2, y2, xA, yA, xB, yB, width, height, croppX1, croppY1,croppX2, croppY2, croppW, croppH;// Local Coordinates
 	
-	int x1, y1, x2, y2;// Local Coordinates
+	String folderPath;
+	String imagePath;
 	
 	/**
 	 * Create the panel.
 	 */
-	public ImageLabel() {
-		icon = new ImageIcon(fileName);
+	public ImageLabel(String filePath) {
+		imagePath = filePath;
+		try {
+			origImage = ImageIO.read(new File(filePath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+		icon = new ImageIcon(filePath);
 		
 		image = icon.getImage();
 		this.setVisible(true);
 		
 		// set coordinates for the Label
 		this.setSize(screenSize);
-		
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
-
+		
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent e) {
-	}
+	public void actionPerformed(ActionEvent e) {}
 	
 	/**
 	 * Sets the starting Coordinates
@@ -66,6 +80,7 @@ public class ImageLabel extends JLabel implements ActionListener, MouseListener,
 	public void setStartPoint(int x, int y) {
         this.x1 = x;
         this.y1 = y;
+        
     }
 	/**
 	 * Sets the ending Coordinates
@@ -84,71 +99,244 @@ public class ImageLabel extends JLabel implements ActionListener, MouseListener,
 	public void mousePressed(MouseEvent e) {
         setStartPoint(e.getX(), e.getY());
         System.out.println("Mouse pressed at ("+e.getX()+","+e.getY()+")");
+        repaint();
     }
 
     public void mouseDragged(MouseEvent e) {
         setEndPoint(e.getX(), e.getY());
-        setCoordinates(x1, y1, x2, y2);
+        setLocalCoordinates(x1, y1, x2, y2);
         repaint();
         
     }
     public void mouseReleased(MouseEvent e) {
         setEndPoint(e.getX(), e.getY());
-        
-        System.out.println("Mouse released at ("+e.getX()+","+e.getY()+")");
-        setCoordinates(x1, y1, x2, y2);
+        setLocalCoordinates(x1, y1, x2, y2);
+        System.out.println("Imgx1: "+drawImg.x1+", Imgy1:"+drawImg.y1+", Imgx2: "+drawImg.x2+", Imgy2:"+drawImg.y2+", ImgWidth: "+drawImg.imgWidth+", ImgHeight: "+drawImg.imgHeight);
+        System.out.println("IconWidth: "+icon.getIconWidth()+", IconHeight: "+icon.getIconHeight());
+		
+        System.out.println("x1: "+x1+", y1: "+y1+", x2: "+x2+", y2: "+y2);
+        System.out.println("xA: "+xA+", yA:"+yA+", xB: "+xB+", yB:"+yB+", width: "+width+", height: "+height);
+
+        repaint();
     }
 	@Override
 	public void mouseMoved(MouseEvent e) {}
 	@Override
-	public void mouseClicked(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {
+		setLocalCoordinates(0,0,0,0);
+		repaint();
+	}
 	@Override
 	public void mouseEntered(MouseEvent e) {}
 	@Override
 	public void mouseExited(MouseEvent e) {}
-	
-	
-    /**
+		
+	/**
+	 * Paint the image and the Rectangles that shows the crop part
+	 */
+	public void paintComponent(Graphics g) {
+		//super.paintComponent(g);
+		//Image 
+		//g.drawImage(image, 0, 0, this);
+		drawImg = new ImageDraw();//(Resize the image based on the canvas) that is given (this)
+		drawImg.drawScaledImage(origImage, this, g);
+        
+		//Rectangles   
+        g.setColor(new Color(128, 128, 128, 80));
+        g.drawRect(xA, yA, width, height);
+        g.fillRect(xA, yA, width, height);
+       
+        if(!isRepainted) {//check if the image is up to date
+        	repaint();
+        	isRepainted=true;
+        }
+    	//repaint();
+        
+	}
+	/**
      * Sets the Coordinates of the Rectangle/Cropped image
      * @param x1
      * @param y1
      * @param x2
      * @param y2
      */
-    public void setCoordinates(int x1, int y1, int x2, int y2) {
+    public void setLocalCoordinates(int x1, int y1, int x2, int y2) {
     	//checking all the possible directions so we dont have width or height under zero
-    	if(x2>x1 && y2>y1) {
-    		coord = new Coordinates(x1, y1, x2-x1 ,y2-y1);
+    	if(x2>x1 && y2>y1) {    	
+    		xA = x1; yA = y1; xB = x2; yB = y2; width = x2-x1; height = y2-y1;
+    	
     	} else if(x2<x1 && y2>y1) {
-        	coord = new Coordinates(x2,y1,x1-x2,y2-y1);
-        } else if( x2>x1 && y2<y1) {
-        	coord = new Coordinates(x1,y2,x2-x1,y1-y2);
-        }else if( x2<x1 && y2<y1) {
-        	coord = new Coordinates(x2,y2,x1-x2,y1-y2);
-        }//////
-    	System.out.println("X: "+coord.getX()+"\nY: "+coord.getY()+"\nWidth: "+coord.getWidth()+"\nHeight: "+coord.getHeight());
-        SwingUtilities.updateComponentTreeUI(this);
-	}
-	/**
-	 * Paint the image and the Rectangles that shows the crop part
-	 */
-	public void paintComponent(Graphics g) {
-		//Image 
-		ImageDraw drawImg = new ImageDraw();//(Resize the image based on the canvas) that is given (this)
-		drawImg.drawScaledImage(icon.getImage(), this, g);
+    		xA = x2; yA = y1; xB = x1; yB = y2; width = x1-x2; height = y2-y1;
         
-		//Rectangles   
-        g.setColor(new Color(128, 128, 128, 80));
-        g.drawRect(coord.getX(), coord.getY(), coord.getWidth(), coord.getHeight());
-        g.fillRect(coord.getX(), coord.getY(), coord.getWidth(), coord.getHeight());
-       
-            
-        repaint();
+    	} else if( x2>x1 && y2<y1) {
+        	xA = x1; yA = y2; xB = x2; yB = y1; width = x2-x1; height = y1-y2;
+
+    	}else if( x2<x1 && y2<y1) {        
+    		xA = x2; yA = y2; xB = x1; yB = y1; width = x1-x2; height = y1-y2;
+    	}
+    	if(x1==0 && y1==0 && x2==0 && y2==0) {
+    		xA =0; yA=0;xB=0;yB=0;width=0;height=0;
+    	}
+    	correctLocalCoordinates();
+    	setCoordinates(xA,yA,width,height);
+
+    	repaint();
+        //SwingUtilities.updateComponentTreeUI(this);
 	}
 	
-	public void CoppTheImage() {
-		croppedImage = createImage(new FilteredImageSource(image.getSource(),
-				new CropImageFilter(coord.getX(), coord.getY(), coord.getWidth(), coord.getHeight())));
+	public void correctLocalCoordinates() {
+		if(xA<drawImg.x1) {
+    		this.xA = drawImg.x1;
+    	}
+    	if(yA<drawImg.y1) {
+    		this.yA = drawImg.y1;
+    	}
+    	if(xB>drawImg.x2) {
+    		this.xB = drawImg.x2;
+    	}
+    	if (yB>drawImg.y2) {
+    		this.yB = drawImg.y2;
+    	}
+    	if(xA<=0) {
+    		xA=0;
+    	}
+    	if(yA<=0) {
+    		yA=0;
+    	}
+    	if(xB<=0) {
+    		xB=0;
+    	}
+    	if(yB<=0) {
+    		yB=0;
+    	}
+    	this.width = xB - xA;
+    	this.height = yB - yA;
+    	if(width<=0) {
+    		width=0;
+    	}
+    	if(height<=0) {
+    		height=0;
+    	}
+    	
+    	setCoordinates(xA,yA,width,height);
+    	
+	}
+	public void setCoordinates(int x, int y, int w,int h){
+		
+		coord.setX(x);
+		coord.setY(y);
+		coord.setWidth(w);
+		coord.setHeight(h);
+	
+	}
+	
+	public void setCroppCoordinates(){
+		/**
+		 * diagonal = squareRoot( (width)^2 + ( heigt)^2)
+		 */
+		
+		System.out.println("getX: "+coord.getX()+", getY: "+coord.getY());
+		if (drawImg.imgWidth < drawImg.canvWidth && drawImg.imgHeight < drawImg.canvHeight) {
+            // when the image is smaller than the canvas
+             croppX1 = coord.getX()-drawImg.x1;
+             croppY1 = coord.getY()-drawImg.y1;
+             croppW = coord.getWidth();
+             croppH = coord.getHeight();
+        } else { 
+        	//calculating the percent that the Diagonal of the image grows
+        	float origImgAspect = (float) icon.getIconHeight() / icon.getIconWidth();
+        	int scaledHeight = drawImg.y2 - drawImg.y1;
+        	int scaledWidth = drawImg.x2 -drawImg.x1;
+
+        	float origDiag = (float) Math.sqrt(Math.pow(icon.getIconHeight(),2) + Math.pow(icon.getIconWidth(),2));
+        	float scaledDiag = (float) Math.sqrt(Math.pow(scaledWidth, 2) + Math.pow(scaledHeight, 2));
+        	
+        	float percentDiagon = origDiag / scaledDiag;
+        	
+        	// correct the rectangle coordinates and dimensions 
+        	if(origImgAspect<1) {
+        		float x;
+        		x = (float) (( coord.getX()-drawImg.x1 ) / percentDiagon);
+        		croppX1 = (int) (x);
+        		x = (float) (( coord.getY()-drawImg.y1 ) / percentDiagon);
+            	croppY1 =  (int) (x);
+            	x = (float) (coord.getWidth() / percentDiagon); 
+            	croppW = (int) (x);
+            	x = (float) (coord.getHeight() / percentDiagon);
+            	croppH = (int) (x);
+        		
+        	}else if(origImgAspect>1) {
+        		float x;
+        		x = (float) (( coord.getX()-drawImg.x1 )  * percentDiagon);
+        		croppX1 = (int) (x);
+        		x = (float) (( coord.getY()-drawImg.y1 )  * percentDiagon);
+            	croppY1 =  (int) (x);
+            	x = (float) (coord.getWidth() * percentDiagon); 
+            	croppW = (int) (x);
+            	x = (float) (coord.getHeight() * percentDiagon);
+            	croppH = (int) (x);
+        	}
+        }
+		
+		
+	}
+	
+	/*
+	 * display cropped image without save 
+	 */
+	public void displayCropped() {
+		setCoordinates(xA,yA,width,height);
+		setCroppCoordinates();
+
+		System.out.println("XXX: " +croppX1 + "YYY: " +croppY1);
+	
+		origImage = origImage.getSubimage(croppX1, croppY1, croppW, croppH);
+		
+		System.out.println("Cropped Image Dimension: "+origImage.getWidth()+"x"+origImage.getHeight());
+		
+		repaint();
+	}
+	
+	public void CroppTheImage() {
+		setCoordinates(xA,yA,width,height);
+		setCroppCoordinates();
+
+		System.out.println("XXX: " +croppX1 + "YYY: " +croppY1);
+		BufferedImage subImage;
+		if(croppX1<=0 && croppY2<=0 && croppW<=0 && croppH<=0) {
+			subImage = origImage;
+
+		}else {
+			subImage = origImage.getSubimage(croppX1, croppY1, croppW, croppH);
+
+		}
+		
+		System.out.println("Cropped Image Dimension: "+subImage.getWidth()+"x"+subImage.getHeight());
+		
+		File outputfile = new File("");
+		
+		for(String tf : typeFiles) {
+			if(folderPath.contains(tf)) {
+				folderPath = folderPath.replace(tf,".jpg");
+				outputfile = new File(folderPath);
+				folderPath = outputfile.getPath();
+				break;
+			}else {
+				folderPath = folderPath + ".jpg";
+				outputfile = new File(folderPath);
+				break;
+			}
+
+		}
+		
+		try {
+			ImageIO.write(subImage, "jpg", outputfile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Image cropped successfully: "+outputfile.getPath());
 	}
 	
 	
